fd1c62e160275d53fb5d21ddfb93749b7f0838cf
Alan Mabry <alan.mabry@gmail.com>
2018-11-28 00:00:36 -0500

    cylcer random starting point = wip

--- src/EntityCycler.js
+++ src/EntityCycler.js
@@ -1,11 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import './EntityCycler.css';
 
 class EntityCycler extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      index: 0,
+      text: [
+        'Apps',
+        'Websites',
+        'Tools',
+        'Landing Pages'
+      ]
+    }
+  }
+  
+  getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+  
+  currentText() {
+    const index = this.state.index || this.getRandomInt(this.state.text.length); 
+
+    return (
+      <Fragment>
+        {this.state.text[index]}
+      </Fragment>
+    )
+  }
+
   render() {
     return (
       <div className="EntityCycler">
-        Apps
+        {this.currentText()}
       </div>
     );
   }
