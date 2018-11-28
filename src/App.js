9b267a5c0a65055126b12166eddbc03985b01ff2
Alan Mabry <alan.mabry@gmail.com>
2018-11-27 23:43:49 -0500

    refactor

--- src/App.js
+++ src/App.js
@@ -1,43 +1,12 @@
-import React, { Component, Fragment } from 'react';
-import EntityCycler from './EntityCycler';
+import React, { Component } from 'react';
 import One from './One';
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      index: 0,
-      text: {
-        0: this.zero(),
-        1: 'tbd'
-      }
-    };
-    this.nextPage = this.nextPage.bind(this);
-  }
-  
-  zero() {
-    return (
-      <Fragment>
-        I ♥︎ building
-        <EntityCycler />
-        and care about
-        <br />
-        user experience
-      </Fragment>
-    );
-  }
-  
-  nextPage() {
-    this.setState(state => ({
-      index: this.state.index + 1
-    }));
-  }
-
   render() {
     return (
       <div className="App">
-        <One text={this.state.text[this.state.index]} nextText={this.nextPage} />
+        <One />
       </div>
     );
   }
--- src/One.js
+++ src/One.js
@@ -1,11 +1,46 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import EntityCycler from './EntityCycler';
 import './One.css';
 
 class One extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      index: 0,
+      text: {
+        0: this.zero(),
+        1: 'tbd'
+      }
+    };
+    this.nextPage = this.nextPage.bind(this);
+  }
+  
+  zero() {
+    return (
+      <Fragment>
+        I ♥︎ building
+        <EntityCycler />
+        and care about
+        <br />
+        user experience
+      </Fragment>
+    );
+  }
+  
+  currentText() {
+    return this.state.text[this.state.index];
+  }
+  
+  nextPage() {
+    this.setState(state => ({
+      index: this.state.index + 1
+    }));
+  }
+
   render() {
     return (
       <div className="One">
-        {this.props.text} <button className="next-page" onClick={this.props.nextText}>...</button>
+        {this.currentText()} <button className="next-page" onClick={this.nextPage}>...</button>
       </div>
     );
   }
